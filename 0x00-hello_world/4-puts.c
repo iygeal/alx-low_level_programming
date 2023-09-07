bf1782b6de8cafaa993757655d39a79dae37ef61
Innocent Anozie <iygeal@gmail.com>
2023-09-07 14:49:00 -0700

    Printing with puts function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - This program prints with the puts function.
+ * Return: 0 (Implies success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
+}
