64d236471a9a736e8ca50bf494c1fcae96599c69
Innocent Anozie <iygeal@gmail.com>
2023-09-07 16:04:31 -0700

    Print sizes on computer

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,8 +11,8 @@ int main(void)
 	long long int c;
 	float f;
 
-	printf("Size of char: %c byte(s)\n", sizeof(char));
-	printf("Size of int: %d byte(s)\n", sizeof(int));
+	printf("Size of char: %zu byte(s)\n", sizeof(char));
+	printf("Size of int: %zu byte(s)\n", sizeof(int));
 	printf("Size of long int: %zu byte(s)\n", sizeof(long));
 	printf("Size of long long int: %zu byte(s)\n", sizeof(long long));
 	printf("Size of float: %zu byte(s)\n", sizeof(float));
