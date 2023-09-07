42b5a5679e4cadcb5ef1b98d2cb8eb34bb69d4a8
Innocent Anozie <iygeal@gmail.com>
2023-09-07 16:02:33 -0700

    Print sizes on system

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - This program prints various sizes on this computer.
+ * Return: 0 (Signifies success)
+ */
+int main(void)
+{
+	char a;
+	int i;
+	long int b;
+	long long int c;
+	float f;
+
+	printf("Size of char: %c byte(s)\n", sizeof(char));
+	printf("Size of int: %d byte(s)\n", sizeof(int));
+	printf("Size of long int: %zu byte(s)\n", sizeof(long));
+	printf("Size of long long int: %zu byte(s)\n", sizeof(long long));
+	printf("Size of float: %zu byte(s)\n", sizeof(float));
+return (0);
+
