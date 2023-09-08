5a605bfc2b45f5a763d14cefb9a4cf9ae69178af
Innocent Anozie <iygeal@gmail.com>
2023-09-07 18:08:45 -0700

    Unix is fascinating

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,7 +4,7 @@
  * main - This program prints to standard error.
  * Return: 1 (Success)
  */
-int main (void)
+int main(void)
 {
 	fput("and that piece of art is useful\" - Dora Korpa, 2015-10-19\n", stdout");
 return (1);
