64d2989df6d818a761d8e84ad80237246d2fd85c
Innocent Anozie <iygeal@gmail.com>
2023-09-08 16:47:22 -0700

    While loop A-Z and a-z

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/** main - This program prints alphabets A-Z and a-z.
+ * Return: 0 = Success
+ */
+int main(void)
+{
+	char x = 'a'
+	char y = 'A'
+
+	while
+	{
+		(x <= z)
+		putchar(x);
+	}
+	while
+	{
+		(y <= Z)
+		putchar(y);
+	}
+	putchar('\n');
+	return (0)
+}
