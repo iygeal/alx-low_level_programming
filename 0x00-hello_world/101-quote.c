d93a0ae8d174eab293515fbd9607b549b1b7440e
Innocent Anozie <iygeal@gmail.com>
2023-09-07 18:04:47 -0700

    UNIX is interesting

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - This program prints to standard error.
+ * Return: 1 (Success)
+ */
+int main (void)
+{
+	fput("and that piece of art is useful\" - Dora Korpa, 2015-10-19\n", stdout");
+return (1);
+}
