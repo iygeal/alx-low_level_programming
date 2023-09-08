b3840f5974b629d1a0a4d94d8d6947699bad5cef
Innocent Anozie <iygeal@gmail.com>
2023-09-08 16:19:20 -0700

    While loop to print all alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - A program that prints alphabets a to z.
+ * Return: 0 = success
+ */
+int main(void)
+{
+	char alphabet;
+
+	alphabet = 'a';
+
+	while
+		(alphabet <= 'z');
+	{
+		putchar(alphabet);
+		alphabet++;
+	}
+		putchar('\n');
+	return (0);
+}
