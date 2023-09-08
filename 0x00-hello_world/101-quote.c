4f5d9e9df10fe1ee4dbcb9ed18d672efae733a56
Innocent Anozie <iygeal@gmail.com>
2023-09-07 18:28:24 -0700

    Unix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,10 +2,10 @@
 #include <unistd.h>
 /**
  * main - This program prints to standard error.
- * Return: 0 (Success)
+ * Return: 1 (Success)
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpa, 2015-10-19\n", 59);
+	fput("and that piece of art is useful\" - Dora Korpa, 2015-10-19\n", stdout);
 	return (1);
 }
