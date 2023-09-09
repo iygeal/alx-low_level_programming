645c49bff049c91b3bbc180de2ce447c7e0adaa7
Innocent Anozie <iygeal@gmail.com>
2023-09-08 22:02:56 -0700

    3 number combos

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,26 @@
+#include <stio.h>
+/**
+ * main - This program makes guided combination of 3 numbers.
+ * Return: 0 = success
+ */
+int main(void)
+{
+	for (int i = 0; i <= 7; i++)
+	{
+		for (int j = i + 1; j <= 8; j++)
+		{
+			for (int k = j + 1; k <= 9; k++)
+			{
+				putchar('0' + i);
+				putchar('0' + j);
+				putchar('0' + k);
+				if (!(i == 7 && j == 8 && k == 9))
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+return (0);
+}
