3c58d39bf9e29058afb186311c7ea9b123adfa7f
Innocent Anozie <iygeal@gmail.com>
2023-09-09 16:42:41 -0700

    Print combination of numbers less than 99

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,20 +10,23 @@ int main(void)
 
 	for (a = 0; a <= 99; a++)
 	{
-		putchar((a / 10) + '0');
-		putchar((a % 10) + '0');
-		putchar(' ');
-		putchar((b / 10) + '0');
-		putchar((b % 10) + '0');
-		if (b != 99)
+		for (b = a; b <= 99; b++)
 		{
-			putchar(',');
+			putchar((a / 10) + '0');
+			putchar((a % 10) + '0');
 			putchar(' ');
-		}
-		else
-		{
-			putchar('\n');
+			putchar((b / 10) + '0');
+			putchar((b % 10) + '0');
+			if (b != 99)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			else
+			{
+				putchar('\n');
+			}
 		}
 	}
-return (0);
+	return (0);
 }
