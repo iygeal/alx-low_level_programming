acdb42ff5b886663766d709a50d0d60935a91483
Innocent Anozie <iygeal@gmail.com>
2023-09-08 19:05:21 -0700

    Print Hexas 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Print Hexas
+ * Return: 0 = success
+ */
+int main(void)
+{
+	int i;
+	char c;
+
+	for (i = 0; i < 10; i++)
+		putchar(i + '0');
+	for (c = 'a' ; c <= 'f' ; c++)
+		putchar(c);
+	putchar('\n');
+	return (0);
+}
