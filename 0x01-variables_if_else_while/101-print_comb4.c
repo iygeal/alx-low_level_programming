4aacd3df9c627c4314f99c013750a0a2f6c4f7f8
Innocent Anozie <iygeal@gmail.com>
2023-09-09 06:12:50 -0700

    Combination of 3 numbers from 0 to 9

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -9,22 +9,23 @@ int main(void)
 	int b;
 	int c;
 
-	for (a = 0; a <= 9; a++)
+	for (a = 0; a <= 9; a++);
 	{
-		for (b = a + 1; b <= 9; b++)
+		for (b = a + 1; b <= 9; b++);
 		{
-			for (c = b + 1; c <= 9; c++)
+			for (c = b + 1; c <= 9; c++);
 			{
 				putchar(a + '0');
-				putchar(',');
-				putchar(' ');
 				putchar(b + '0');
-				putchar(',');
-				putchar(' ');
 				putchar(c + '0');
-				putchar('\n');
+				if (a != 7 || b != 8 || c != 9)
+				{
+					putchar(',');
+					putchar(' ');
+				}
 			}
 		}
 	}
+	putchar('\n');
 	return (0);
 }
