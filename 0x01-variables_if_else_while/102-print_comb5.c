cc710b3621f8ae8bf6719bce61255281bb62d246
Innocent Anozie <iygeal@gmail.com>
2023-09-09 16:01:04 -0700

    More number combinations

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - This program prints combination of double digit numbers
+ * Return: 0 = success!
+ */
+int main(void)
+{
+	int a;
+	int b;
+
+	for (a = 0; a <= 99; a++)
+	{
+		putchar((a / 10) + '0');
+		putchar((a % 10) + '0');
+		putchar(' ');
+		putchar((b / 10) + '0');
+		putchar((b % 10) + '0');
+		if (b != 99)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		else
+		{
+			putchar('\n');
+		}
+	}
+return (0);
+}
