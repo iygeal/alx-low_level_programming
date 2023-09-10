d5b2a72e740e2300254c44518fe48c88ca1a4a35
Innocent Anozie <iygeal@gmail.com>
2023-09-10 03:23:15 -0700

    Print double digit numbers

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,23 +10,23 @@ int main(void)
 
 	for (a = 0; a <= 99; a++)
 	{
-		for (b = a; b <= 99; b++)
+		for (b = 0; b <= 99; b++)
 		{
-			putchar((a / 10) + '0');
-			putchar((a % 10) + '0');
-			putchar(' ');
-			putchar((b / 10) + '0');
-			putchar((b % 10) + '0');
-			if (b != 99)
+			if (a < b)
 			{
+				putchar((a / 10) + '0');
+				putchar((a % 10) + '0');
+				putchar(' ');
+				putchar((b / 10) + '0');
+				putchar((b % 10) + '0');
+				if (a != 98 || b != 99)
+				{
 				putchar(',');
 				putchar(' ');
-			}
-			else
-			{
-				putchar('\n');
-			}
+				}
+			}			
 		}
 	}
+	putchar('\n');
 	return (0);
 }
