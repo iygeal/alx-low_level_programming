c8876efd78596a3d667bfed530531d02906e5861
Innocent Anozie <iygeal@gmail.com>
2023-09-12 23:42:19 +0100

    Program that prints absolute value of integers

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _abs - Prints absolute value of integer.
+ *@x: The integer to be checked and printed.
+ * Return: 0 = success
+ */
+int _abs(int x)
+{
+	if (x > 0)
+		return (-x);
+	else if (x >= 0)
+		return (x);
+
+return (0);
+}
