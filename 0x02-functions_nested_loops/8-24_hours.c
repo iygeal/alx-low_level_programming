c15481dadf99f528d4c6bf6e7bfe824811d49392
Innocent Anozie <iygeal@gmail.com>
2023-09-13 00:25:24 +0100

    This program prints every minute and hour of the day

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * jack_bauer - Function that prints minutes and hours
+ *
+ */
+void jack_bauer(void)
+{
+	int x, y;
+
+	for (x = 0 ; x < 24 ; x++)
+	{
+		for (y = 0 ; y < 60 ; y++)
+		{
+			_putchar(x / 10 + 48);
+			_putchar(x % 10 + 48);
+			_putchar(':');
+			_putchar(y / 10 + 48);
+			_putchar(y % 10 + 48);
+		}
+	}
+}
