da106cb7496cd5dbefa7706f98939fa392b2a0ad
Innocent Anozie <iygeal@gmail.com>
2023-09-13 02:17:11 +0100

    9 Times Table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,34 +7,30 @@ void times_table(void)
 {
 	int a, b, outcome;
 
-	for (a = 0; a <= 9; a++)
+	for (a = 0; a  < 10; a++)
 	{
-		for (b = 0; b <= 9; b++)
+		for (b = 0; a < 10; b++)
 		{
-			outcome = a * b;
-
-			if (outcome < 10)
+			outcome = b * a;
+			if (b == 0)
 			{
-				_putchar(' ');
 				_putchar(outcome + '0');
 			}
-			else
-			{
-				_putchar(outcome / 10 + '0');
-				_putchar(outcome % 10 + '0');
-			}
 
-			if (b < 9)
+			if (outcome < 10 && b != 0)
 			{
 				_putchar(',');
 				_putchar(' ');
-			}
-			else
+				_putchar(' ');
+				_putchar(outcome + '0');
+			} else if (outcome >= 10)
 			{
-				_putchar('\n');
+				_putchar(',');
+				_putchar(' ');
+				_putchar((outcome / 10) + '0');
+				_putchar((outcome % 10) + '0');
 			}
 		}
+		_putchar('\n');
 	}
-
 }
-
