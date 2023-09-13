577823206e360f5134c1ad5cb2ceae431f73f477
Innocent Anozie <iygeal@gmail.com>
2023-09-13 08:07:30 +0100

    104 Fibo Nacci

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,64 +1,38 @@
 #include <stdio.h>
-
-
 /**
- * numLength - returns the lenth of string
- * @num : operand number
- * Return: number of digits
+ * main - prints the first 98 numbers in a fibonacci sequence
+ * Return: 0.
  */
-
-int numLength(int num)
-{
-	int length = 0;
-
-	if (!num)
-	{
-		return (1);
-	}
-
-	while (num)
-	{
-		num = num / 10;
-		length += 1;
-	}
-
-	return (length);
-}
-/**
- * main - prints the first 98 fibonaci sequences
- * Return: 0
- */
-
 int main(void)
 {
-	unsigned long f1 = 1, f2 = 2, tmp, mx = 100000000, f1o = 0, f2o = 0, tmpo = 0;
-	short int i = 1, initial0s;
-
-	while (i <= 98)
-	{
-		if (f1o > 0)
-			printf("%lu", f1o);
-		initial0s = numLength(mx) - 1 - numLength(f1);
-		while (f1o > 0 && initial0s > 0)
-		{
-			printf("%i", 0);
-			initial0s--;
-		}
-		printf("%lu", f1);
-
-		tmp = (f1 + f2) % mx;
-		tmpo = f1o + f2o + (f1 + f2) / mx;
-		f1 = f2;
-		f1o = f2o;
-		f2 = tmp;
-		f2o = tmpo;
-
-		if (i != 98)
-			printf(", ");
-		else
-			printf("\n");
-		i++;
-	}
-	return (0);
+unsigned long int i;
+unsigned long int fir = 1;
+unsigned long int sec = 2;
+unsigned long int l = 1000000000;
+unsigned long int bef1;
+unsigned long int bef2;
+unsigned long int aft1;
+unsigned long int aft2;
+printf("%lu", fir);
+for (i = 1; i < 91; i++)
+{
+printf(", %lu", sec);
+sec += fir;
+fir = sec - fir;
 }
-
+bef1 = (fir / l);
+bef2 = (fir % l);
+aft1 = (sec / l);
+aft2 = (sec % l);
+for (i = 92; i < 99; ++i)
+{
+printf(", %lu", aft1 + (aft2 / l));
+printf("%lu", aft2 % l);
+aft1 = aft1 + bef1;
+bef1 = aft1 - bef1;
+aft2 = aft2 + bef2;
+bef2 = aft2 - bef2;
+}
+printf("\n");
+return (0);
+}
