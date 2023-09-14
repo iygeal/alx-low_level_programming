4e142106a46696dfaab2cc72f0d021eb386958f2
Innocent Anozie <iygeal@gmail.com>
2023-09-14 21:26:17 +0100

    More numbers printed and repeated

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * more_numbers - Prints same numbers looped in 10 lines
+ * Return: 0 Success
+ */
+void more_numbers(void)
+{
+	i = 0;
+	while (i < 10)
+	{
+		j = 0;
+		while (j < 14)
+		{
+			_putchar(j + 48);
+		}
+		_putchar('\n');
+		i++;
+	}
+	_putchar()'\n');
+}
