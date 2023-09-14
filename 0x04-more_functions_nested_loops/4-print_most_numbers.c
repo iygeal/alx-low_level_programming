5efb74e1e1a9a1f342d8cbf2a22d5596c7952da0
Innocent Anozie <iygeal@gmail.com>
2023-09-14 18:57:46 +0100

    Print select numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_most_numbers - Function that prints most numbers
+ * Return: 0 is success
+ */
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0 ; i <= 9 ; i++)
+	{
+		if (i != 2 || i != 4)
+			_putchar(i + 48);
+	}
+}
