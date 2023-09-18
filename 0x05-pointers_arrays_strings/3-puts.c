b12104276d1c7a2534ed7131d626faeb096ad255
Innocent Anozie <iygeal@gmail.com>
2023-09-18 21:49:27 +0100

    Print string with _puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _puts - Prints a string
+ * @str: String to be printed
+ * Return: 0 = success
+ */
+void _puts(char *str)
+{
+	char *str;
+
+	_puts(str);
+	return (0);
+}
