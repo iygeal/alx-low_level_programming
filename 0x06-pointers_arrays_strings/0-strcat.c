daba7a380a0be06761e262ff5e498c7f86040c37
Innocent Anozie <iygeal@gmail.com>
2023-09-20 19:27:25 +0100

    Strings Concatenated

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -3,8 +3,21 @@
  * _strcat - String Concatenator
  * @dest: Destination string
  * @src: Source string
+ * Return: destlen
  */
 char *_strcat(char *dest, char *src)
 {
+	char *destlen = dest;
 
+	while (*dest != '\0')
+		dest++;
+	while (*src != '\0')
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+	*dest = '\0';
+
+	return (destlen);
 }
