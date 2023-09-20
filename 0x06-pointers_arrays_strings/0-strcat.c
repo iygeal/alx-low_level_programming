ca91e527994fa0e32c3276d6cd7fbd09730fa78d
Innocent Anozie <iygeal@gmail.com>
2023-09-20 13:35:34 +0100

    String Concatenator

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _strcat - String Concatenator
+ * @dest: Destination string
+ * @src: Source string
+ */
+char *_strcat(char *dest, char *src)
+{
+
+}
---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,3 @@
+This directory is for projects done on more pointers, arrays and strings.
+The journey into C programming is getting more seriously difficult.
+We can do it!
