bda366c6c0e7d3fdf3083ece57e35c9aae3af0a7
Innocent Anozie <iygeal@gmail.com>
2023-09-22 01:44:37 +0100

    Capitalize string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,34 +1,38 @@
 #include "main.h"
 /**
- * cap_ntring - Function that capitaizen all contentn of a ntring
- * @n: The ntring
+ * cap_string - Function that capitalizes all contents of a string
+ * @n: The string
  * Return: 0
  */
-char *cap_ntring(char *n)
+char *cap_string(char *n)
 {
-	int index = 0;
+	int len = 1, j;
+	char c[] = ",;.!?\"(){}";
 
-	while (n[index])
+	if (n[0] > 96 && n[0] < 123)
+		n[0] -= 32;
+
+
+	while (n[len] != '\0')
 	{
-		while (!(n[index] >= 'a' && n[index] <= 'z'))
-			index++;
+		int t = n[len - 1];
 
-		if (n[index - 1] == ' ' ||
-		n[index - 1] == '\t' ||
-		n[index - 1] == '\n' ||
-		n[index - 1] == ',' ||
-		n[index - 1] == ';' ||
-		n[index - 1] == '.' ||
-		n[index - 1] == '!' ||
-		n[index - 1] == '?' ||
-		n[index - 1] == '"' ||
-		n[index - 1] == '(' ||
-		n[index - 1] == ')' ||
-		n[index - 1] == '{' ||
-		n[index - 1] == '}' ||
-		index == 0)
-			n[index] -= 32;
-		index++;
+		if (t == 32 || t == 9 || t == 10)
+		{
+			if (n[len] > 96 && n[len] < 123)
+				n[len] -= 32;
+		}
+		for (j = 0; j < 10; j++)
+		{
+			if (t == (int) c[j])
+			{
+				if (n[len] > 96 && n[len] < 123)
+					n[len] -= 32;
+				break;
+			}
+		}
+		len++;
 	}
+
 	return (n);
 }
