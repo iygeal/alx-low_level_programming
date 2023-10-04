d187cff69a50603c2a7bb76c99dbb23af6c83ed6
Innocent Anozie <iygeal@gmail.com>
2023-10-04 20:42:58 +0100

    Implementation of the strdup function, a string duplicator

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * _strdup - Function that returns a pointer to newly alocated space in memory
+ * @str: A string
+ * Return: Returns new string or NULL
+ */
+char *_strdup(char *str)
+{
+	int i;
+	int j;
+	int n = 0;
+	char *str2;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		n++;
+	}
+	str2 = malloc(n * sizeof(char) + 1);
+
+	for (j = 0; j < n; j++)
+	{
+		str2[j] = str[j];
+	}
+	str2[j] = '\0';
+return (str2);
+}
