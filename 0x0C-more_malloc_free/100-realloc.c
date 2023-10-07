bb4800d96f17f5d417bbc4968e0d1fab3dc41a70
Innocent Anozie <iygeal@gmail.com>
2023-10-07 22:10:31 +0100

    Implementing code that mimics the realloc function in C

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,38 @@
+#include "main.h"
+/**
+ * _realloc - Function that reallocates memory using malloc
+ * @ptr: Pointer to old allocated memory
+ * @old_size: Size in bytes for ptr
+ * @new_size: Size in bytes for the new memory block
+ * Return: ptr or NULL
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	char *ptr2;
+	unsigned int i, _min = 0;
+
+	if (ptr == NULL)
+		return (malloc(new_size));
+	if (new_size == 0)
+		{
+			free(ptr);
+			return (NULL);
+		}
+	if (new_size == old_size)
+		return (ptr);
+	ptr2 = malloc(new_size);
+	if (ptr2 == NULL)
+		return (NULL);
+	if (new_size < old_size)
+	{
+		_min = new_size;
+	}
+	else
+	{
+		_min = old_size;
+	}
+	for (i = 0; i < _min; i++)
+		ptr2[i] = ptr[i];
+	free(ptr);
+return (ptr2);
+}
