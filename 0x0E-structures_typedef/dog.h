cff82a465e917191b6fbf20497eef7ee1489f650
Innocent Anoziee <iygeal@gmail.com>
2023-10-10 16:56:22 +0100

    Basic syntax of struct header file

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - Struct for my doggie dog
+ * @name: Name of my puppy
+ * @age: Age of my puppy
+ * @owner: I am the owner
+ */
+
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-}
+};
+
+#endif
