99d5722f83911b193295b5616c4f2d33866baccc
Innocent Anozie <iygeal@gmail.com>
2023-10-10 23:35:23 +0100

    Printing the name of a file a program is compiled from

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+/**
+ * main - Function that prints name of file a program is compiled from.
+ * Return: 0 = Success
+ */
 int main(void)
 {
 	printf("%s\n", __FILE__);
