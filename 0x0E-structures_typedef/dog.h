97748e70b0cea9594fea033210d080ae796bef84
Innocent Anozie <iygeal@gmail.com>
2023-10-10 19:02:12 +0100

    Printing a struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,6 +2,7 @@
 #define DOG_H
 #include <stdio.h>
 #include <stdlib.h>
+
 /**
  * struct dog - Struct for my doggie dog
  * @name: Name of my puppy
@@ -17,7 +18,5 @@ struct dog
 };
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
 
 #endif
