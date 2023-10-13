2406b50f79a8b0c254b85e65ec5f8f83a0b2ba98
Innocent Anozie <iygeal@gmail.com>
2023-10-13 06:59:25 +0100

    Printing all common data types with variadic functions

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,47 @@
+#include "variadic_functions.h"
+/**
+ * print_all - Function that prints all types of arguments passed
+ * @format: Data formats/types passed to be printed
+ * Return: void
+ */
+
+void print_all(const char * const format, ...)
+{
+	unsigned int i = 0;
+	char *str, *separator = "";
+	va_list myFormats;
+
+	va_start(myFormats, format);
+
+	while (format && format[i])
+	{
+		switch (format[i])
+		{
+			case 'c':
+				printf("%s%c", separator, va_arg(myFormats, int));
+				break;
+			case 'i':
+				printf("%s%d", separator, va_arg(myFormats, int));
+				break;
+			case 'f':
+				printf("%s%f", separator, va_arg(myFormats, double));
+				break;
+			case 's':
+				str = va_arg(myFormats, char *);
+				if (str == NULL)
+					printf("%s(nil)", separator);
+				else
+					printf("%s%s", separator, str);
+				break;
+			default:
+				separator = "";
+				i++;
+				continue;
+		}
+		separator = ", ";
+		i++;
+	}
+	va_end(myFormats);
+	printf("\n");
+
+}
