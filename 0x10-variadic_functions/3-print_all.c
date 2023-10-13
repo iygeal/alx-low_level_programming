bfa342e9c3ff7fa9addc36ae3da58e40f8201800
Innocent Anozie <iygeal@gmail.com>
2023-10-13 08:50:52 +0100

    Print all common data types with variadic functions

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -30,7 +30,10 @@ void print_all(const char * const format, ...)
 				case 's':
 					str = va_arg(myFormats, char *);
 					if (str == NULL)
+					{
 						str = "(nil)";
+						printf("%s", str);
+					}
 					printf("%s%s", separator, str);
 					break;
 				default:
