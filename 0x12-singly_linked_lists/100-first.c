0da0bdebacd7b720f610ec14579fd8f067bffeb1
Innocent Anozie <iygeal@gmail.com>
2023-10-20 02:04:53 +0100

    Using __attribute__((constructor)) to execute a function before main function

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+/**
+ * pre_print - Function that runs before main
+ * Return: Void
+ */
+__attribute__((constructor))
+void pre_print(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
