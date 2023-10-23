8612fd24a161ad366d62334799bc7a9e01968738
Innocent Anozie <iygeal@gmail.com>
2023-10-23 22:08:36 +0100

    Free list using pointer to pointer to first element in the list

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * free_listint2 - Function that frees listint_t list
+ * @head: Pointer to the first element in the list
+ * Return: Void
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *p;
+
+	while (*head != NULL)
+	{
+		p = *head;
+		*head = p->next;
+		free(p);
+	}
+	*head = NULL;
+}
