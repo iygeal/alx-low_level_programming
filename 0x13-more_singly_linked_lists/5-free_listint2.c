3591187998ca2b3641d269298998e3a7dc81b3ed
Innocent Anozie <iygeal@gmail.com>
2023-10-23 22:18:02 +0100

    Free list using pointer to pointer to first element in the list

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -10,9 +10,9 @@ void free_listint2(listint_t **head)
 
 	while (*head != NULL)
 	{
-		p = *head;
-		*head = p->next;
-		free(p);
+		p = (*head)->next;
+		free(*head);
+		*head = p;
 	}
 	*head = NULL;
 }
