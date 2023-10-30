9bad05e575fcd885f598987ea078a2e0bfb72694
Innocent Anozie <iygeal@gmail.com>
2023-10-30 23:53:44 +0100

    Function that mimics the cp command

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -24,7 +24,7 @@ int main(int argc, char *argv[])
 	file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (file_to == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", argv[2]);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		exit(99);
 	}
 	while ((num_read = read(file_from, buffer, sizeof(buffer))) > 0)
