47d2b7e3f54cbe9e646fd6e084d03a039aca3506
Innocent Anozie <iygeal@gmail.com>
2023-10-30 21:44:21 +0100

    Function that appends text at the end of an already existing text file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -20,7 +20,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	}
 	if (text_content != NULL)
 	{
-			written = write(fd, text_content, strlen(text_content));
+		written = write(fd, text_content, strlen(text_content));
 	}
 	close(fd);
 	if (written == -1)
