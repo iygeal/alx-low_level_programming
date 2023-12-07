ed4a4c3f0a7613c2cb1ac8dfba463799d235cd59
Innocent Anozie <iygeal@gmail.com>
2023-12-07 21:18:40 +0100

    Function that returns the number of elements in a list

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * dlistint_len - Returns the numbers of elements in a doubly linked list
+ * @h: Pointer to the first element in the list
+ * Return: Returns len if success or NULL if not
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	int counter = 0;
+	
+	if (h == NULL)
+		return (0);
+	while (h != NULL)
+	{
+		h = h->next;
+		counter++;
+	}
+	return (counter);
+}
