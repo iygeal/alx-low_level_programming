ee9831d17f208ecaaa4e9b3053b8ebf0971ca44d
Innocent Anozie <iygeal@gmail.com>
2023-12-11 15:03:06 +0100

    Created Dynamic Library and used it in python

---
+++ 0x18-dynamic_libraries/funcs_for_python.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+
+/**
+ * add - Function that returns sum of two integers
+ * @a: First integer
+ * @b: Second integer
+ * Return: Sum of the integers
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - Function that returns the difference between two integers
+ * @a: First integer
+ * @b: Second integer
+ * Return: Returns the difference
+ */
+
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - Function that returns the product of two integers
+ * @a: First integer
+ * @b: Second integer
+ * Return: Returns the product
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - Function that returns ratio of one integer to another
+ * @a: First integer
+ * @b: Second integer
+ * Return: Ratio
+ */
+
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - Function that returns the modulus between two integers
+ * @a: First integer
+ * @b: Second integer
+ * Return: Modulus
+ */
+
+int mod(int a, int b)
+{
+	return (a % b);
+}
