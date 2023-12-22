2d2337b7cf56c45fbd1813bb00aa175715b74e39
Innocent Anoziee <iygeal@gmail.com>
2023-12-22 17:11:11 +0100

    Saved for later

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,19 @@
+#include "hash_tables.h"
+/**
+* hash_table_set - Function that adds an element to the hash table
+* @ht: Pointer to The hash table
+* @key: The key to be added
+* @value: The hash value associated with the key
+* Return: Returns 1 on success or 0 otherwise
+*/
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	hash_table_t *element;
+
+	element = malloc(sizeof(hash_table_t);
+	if (element == NULL)
+		return (NULL)
+	while (ht && key && value)
+	{
+
+
