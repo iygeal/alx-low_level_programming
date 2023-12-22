beee7f85ebfe410f49ef9841e2fc4c659c843d8f
Innocent Anozie <iygeal@gmail.com>
2023-12-22 06:57:59 +0100

    Created a function that creates a hash table of size @size

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,22 @@
+#include "hash_tables.h"
+/**
+ * hash_table_create - Creates a hash table of size @size
+ * @size: Size of the array of hash tables
+ * Return: Returns pointer to the new hash table
+*/
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *my_hash_table;
+
+	my_hash_table = malloc(sizeof(hash_table_t));
+	if (my_hash_table == NULL)
+		return (NULL);
+	my_hash_table->array = calloc(size, sizeof(hash_node_t *));
+	if (my_hash_table->array == NULL)
+	{
+		free(my_hash_table);
+		return (NULL);
+	}
+	my_hash_table->size = size;
+	return (my_hash_table);
+}
